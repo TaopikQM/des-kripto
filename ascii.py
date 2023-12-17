140523c91336a3f67e7fe862115edb63d38680cc
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-18 00:00:10 +0700

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -11,19 +11,32 @@ def string_to_bin(input):
 def main():
     st.title("String to Binary Converter")
 
+    # Meminta input dari pengguna untuk plaintext
+    plaintext = st.text_input("Masukkan plaintext (kunci):")
+
     # Meminta input dari pengguna
     my_string = st.text_input("Masukkan string Anda:")
 
-    if my_string:
+    if my_string and plaintext:
+        # Mengubah plaintext menjadi representasi biner kunci
+        key_bin_string = string_to_bin(plaintext)
+
         # Mengubah string menjadi representasi biner
         bin_string = string_to_bin(my_string)
 
         # Memisahkan string biner menjadi blok 8 bit
         bin_chunks = list(chunks(bin_string, 8))
 
-        # Mencetak blok 8 bit
-        st.write("Hasil:")
+        # Memisahkan string biner kunci menjadi blok 8 bit
+        key_bin_chunks = list(chunks(key_bin_string, 8))
+
+        # Mencetak blok 8 bit untuk plaintext dan kunci
+        st.write("Hasil untuk Plaintext:")
         for chunk in bin_chunks:
+            st.write(chunk)
+
+        st.write("Hasil untuk Kunci:")
+        for chunk in key_bin_chunks:
             st.write(chunk)
 
 if __name__ == "__main__":
