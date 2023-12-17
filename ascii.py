c132e61ea880c296a1663c0ed7627c0a434d2919
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-18 00:07:00 +0700

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -11,33 +11,38 @@ def string_to_bin(input):
 def main():
     st.title("String to Binary Converter")
 
-    # Meminta input dari pengguna untuk plaintext
+    # Meminta input dari pengguna untuk plaintext (kunci)
     plaintext = st.text_input("Masukkan plaintext (kunci):")
 
-    # Meminta input dari pengguna
-    my_string = st.text_input("Masukkan string Anda:")
+    # Meminta input dari pengguna menggunakan text_area agar dapat memasukkan beberapa string sekaligus
+    my_strings = st.text_area("Masukkan beberapa string (pisahkan dengan baris):")
 
-    if my_string and plaintext:
+    if my_strings and plaintext:
         # Mengubah plaintext menjadi representasi biner kunci
         key_bin_string = string_to_bin(plaintext)
-
-        # Mengubah string menjadi representasi biner
-        bin_string = string_to_bin(my_string)
-
-        # Memisahkan string biner menjadi blok 8 bit
-        bin_chunks = list(chunks(bin_string, 8))
 
         # Memisahkan string biner kunci menjadi blok 8 bit
         key_bin_chunks = list(chunks(key_bin_string, 8))
 
-        # Mencetak blok 8 bit untuk plaintext dan kunci
-        st.write("Hasil untuk Plaintext:")
-        for chunk in bin_chunks:
-            st.write(chunk)
+        # Membagi string menjadi list berdasarkan baris
+        strings_list = my_strings.split('\n')
 
+        # Mencetak blok 8 bit untuk kunci
         st.write("Hasil untuk Kunci:")
         for chunk in key_bin_chunks:
             st.write(chunk)
+
+        # Mencetak blok 8 bit untuk setiap string
+        st.write("Hasil untuk Setiap String:")
+        for my_string in strings_list:
+            # Mengubah string menjadi representasi biner
+            bin_string = string_to_bin(my_string)
+
+            # Memisahkan string biner menjadi blok 8 bit
+            bin_chunks = list(chunks(bin_string, 8))
+
+            # Mencetak blok 8 bit untuk setiap string
+            st.write(bin_chunks)
 
 if __name__ == "__main__":
     main()
