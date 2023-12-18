811c479055b608dcfbd894288cc2b5388818d60a
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-19 00:39:48 +0700

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -152,10 +152,18 @@ class DES:
         return permuted_bit_string
 
     def s_box_substitution(self, bit_string, s_box_index):
-       
+        # Pastikan bit_string memiliki panjang minimal 6 karakter
+        if len(bit_string) < 6:
+            raise ValueError("bit_string harus memiliki panjang minimal 6 karakter")
+    
+        # Lakukan substitusi sesuai dengan indeks yang diinginkan
         row = int(bit_string[0] + bit_string[5], 2)
         col = int(bit_string[1:5], 2)
-        return format(S_Boxes[s_box_index][row][col], '04b')  # 4-bit binary representation
+        
+        return format(S_Boxes[s_box_index][row][col], '04b')
+        #row = int(bit_string[0] + bit_string[5], 2)
+        #col = int(bit_string[1:5], 2)
+        #return format(S_Boxes[s_box_index][row][col], '04b')  # 4-bit binary representation
 
 def string_to_bin(input):
     return ''.join(format(ord(i), '08b') for i in input)
