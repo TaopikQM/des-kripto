8c7baf32415a53aca271f261089184b8807a3526
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-18 19:59:34 +0700

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -218,29 +218,29 @@ def main():
             my_string = st.text_input("Masukkan PLAINTEXT Anda:")
         
             if my_string:
-            # Menampilkan hasil ASCII dari string yang dimasukkan
-            st.subheader("Hasil ASCII:")
-            ascii_result = ' '.join([format(ord(char), '08b') + f" ({char})" for char in my_string])
-            st.write(ascii_result)
-        
-            bin_string = string_to_biin(my_string)  # Pastikan string_to_biin telah didefinisikan
-            if len(bin_string) == 64:  # Gunakan bin_string bukan my_string
-                st.subheader("Tahapan DES PLAINTEXT Generation")
-        
-                my_des = DES(bin_string)
-                my_des.plaintext = bin_string  # Pastikan plaintext diatur dengan benar
-        
-                # Menerapkan Initial Permutation ke plaintext
-                permuted_plaintext = my_des.initial_permutation()
-        
-                # Menampilkan plaintext setelah Initial Permutation
-                st.subheader("Plaintext setelah IP:")
-                st.write(permuted_plaintext)
-        
-                # Memisahkan permuted_plaintext menjadi per 8 bit
-                permuted_plaintext_8bit = [permuted_plaintext[i:i + 8] for i in range(0, len(permuted_plaintext), 8)]
-                st.subheader("Plaintext setelah IP per 8 bit:")
-                st.write(permuted_plaintext_8bit)
+                # Menampilkan hasil ASCII dari string yang dimasukkan
+                st.subheader("Hasil ASCII:")
+                ascii_result = ' '.join([format(ord(char), '08b') + f" ({char})" for char in my_string])
+                st.write(ascii_result)
+            
+                bin_string = string_to_biin(my_string)  # Pastikan string_to_biin telah didefinisikan
+                if len(bin_string) == 64:  # Gunakan bin_string bukan my_string
+                    st.subheader("Tahapan DES PLAINTEXT Generation")
+            
+                    my_des = DES(bin_string)
+                    my_des.plaintext = bin_string  # Pastikan plaintext diatur dengan benar
+            
+                    # Menerapkan Initial Permutation ke plaintext
+                    permuted_plaintext = my_des.initial_permutation()
+            
+                    # Menampilkan plaintext setelah Initial Permutation
+                    st.subheader("Plaintext setelah IP:")
+                    st.write(permuted_plaintext)
+            
+                    # Memisahkan permuted_plaintext menjadi per 8 bit
+                    permuted_plaintext_8bit = [permuted_plaintext[i:i + 8] for i in range(0, len(permuted_plaintext), 8)]
+                    st.subheader("Plaintext setelah IP per 8 bit:")
+                    st.write(permuted_plaintext_8bit)
 
 
 
