7cbe22cc8ecc9c244a429033310ae7ae4d8f934b
TaopikQM <92729541+TaopikQM@users.noreply.github.com>
2023-12-19 18:25:30 +0700

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -318,11 +318,11 @@ def main():
                         st.write(f"Block {i + 1}: {block} -> S-Box Substitution: {s_box_result}")
                     
                     # gabungkan semua hasil menjadi satu baris dengan spasi sebagai pemisah
-                    #single_line = ' '.join(s_box_results)
+                    single_line = ''.join(s_box_results)
                     #st.write(single_line)  
 
                     # asumsikan my_des.permutation sudah didefinisikan
-                    permuted_result = my_des.permutation(single_line.replace(' ', ''))  # hapus spasi sebelum melakukan permutasi
+                    permuted_result = my_des.permutation(single_line)  # hapus spasi sebelum melakukan permutasi
                     
                     # tampilkan hasil permutasi
                     #st.write("Hasil permutasi:", permuted_result)
